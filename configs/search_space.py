4de567dfdbbcd11a5a7a3b9975daf81f7ebc646a
Wei-Chung Liao <wchngliao@gmail.com>
2018-10-09 04:13:30 +0800

    Change search space format

--- configs/search_space.py
+++ configs/search_space.py
@@ -4,8 +4,8 @@ from collections import namedtuple
 with open('configs/search_space.yaml', 'r') as f:
     configs = yaml.load(f)
 
-Layer = namedtuple('Layer', configs['separate'][0].keys())
+Layer = namedtuple('Layer', list(configs[0].keys()) + ['share'])
 
-search_space_separate = [Layer(*layer.values()) for layer in configs['separate']]
-search_space_shared = [Layer(*layer.values()) for layer in configs['shared']]
+search_space_separate = [Layer(*layer.values(), False) for layer in configs]
+search_space_shared = [Layer(*layer.values(), True) for layer in configs]
 search_space_full = search_space_separate + search_space_shared
